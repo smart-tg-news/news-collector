d57c24c9b983c7ca557c5bc7ab24720e028e1be2
Dmitry Kolobaev <kol-di@yandex.ru>
2025-07-05 13:57:54 +0300

    handle environment for mongo client

--- db/client.py
+++ db/client.py
@@ -3,19 +3,27 @@ import os
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
+from typing import Callable
+
+
+class MissingMongoEnv(Exception):
+    pass
+
 
 class MongoClientSingleton:
     _client: MongoClient = None
+    _uri_template: Callable[..., str] = \
+        "mongodb+srv://{mongo_user}:{mongo_pass}@cluster0.1wshziu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0".format
 
     @classmethod
-    def init(cls, uri: str = "mongodb://localhost:27017/", **kwargs):
+    def init(cls, **kwargs):
         """
         Initialize the singleton MongoClient. Can be called once at startup.
         """
-        mongo_user = os.getenv('MONGO_USER')
-        mongo_pass = os.getenv('MONGO_PASS')
-        print(f"Mongo user {mongo_user}; momgo_pass {mongo_pass}")
-        uri = f"mongodb+srv://{mongo_user}:{mongo_pass}@cluster0.1wshziu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+        mongo_user = cls.get_mongo_env_var('MONGO_USER')
+        mongo_pass = cls.get_mongo_env_var('MONGO_PASS')
+        uri = cls._uri_template(mongo_user=mongo_user, mongo_pass=mongo_pass)
+
         if cls._client is None:
             cls._client = MongoClient(
                 uri, 
@@ -29,6 +37,14 @@ class MongoClientSingleton:
         if cls._client:
             cls._client.close()
             cls._client = None
+
+    @staticmethod
+    def get_mongo_env_var(var_name):
+        try:
+            env_var = os.environ[var_name]
+        except KeyError:
+            raise MissingMongoEnv(f"Variable {var_name} doesn't exist")
+        return env_var
 
     def __new__(cls, *args, **kwargs):
         """
